10bbd4f8eb14c1e676f69dbab46b19fb0d28438b
Jeremy Parker <jrobinparker@gmail.com>
2018-06-12 14:43:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,6 @@ def long_planeteer_calls(planeteer_calls = ["earth", "wind", "fire", "water", "h
 end
 
 def find_the_cheese(cheese_types = ["cheddar", "gouda", "camembert"])
-  cheese_types.
+  cheese_types.find do |cheese|
+    if cheese === ""
 end
