6d7863a8cda7d1aedad5c92679cda89b9ab5cbe8
Jeremy Parker <jrobinparker@gmail.com>
2018-06-12 14:38:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 
 def long_planeteer_calls(planeteer_calls = ["earth", "wind", "fire", "water", "heart"])
   planeteer_calls.select do |planeteer|
-    
+    if planeteer.length
 end
 
 def find_the_cheese# code an argument here
