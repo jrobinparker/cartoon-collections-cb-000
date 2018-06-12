df1db77f6aa8b2eea673052f6f9ee382f059835e
Jeremy Parker <jrobinparker@gmail.com>
2018-06-12 14:44:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,7 @@ def long_planeteer_calls(planeteer_calls = ["earth", "wind", "fire", "water", "h
 end
 
 def find_the_cheese(cheese_types = ["cheddar", "gouda", "camembert"])
-  cheese_types.include? do |cheese|
-    if cheese = ""
+  if cheese_types.include?("gouda")
+    return true 
+  else 
 end
